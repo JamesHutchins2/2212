cb002bef4390e806d987f11470ddfd3f37f841ea
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-13 14:40:36 -0500

    Update Document.java
    first edits, adding loops for making linked lists

--- Backend/Document.java
+++ Backend/Document.java
@@ -1,10 +1,40 @@
-Class Document extends Word_Object{
+public class Document extends Word_Object{
 //this class is to work as the hub for the spell check system creating the linked list from the workspace text.
 //It transforms the data types into our word object so that the words can be analyzed by the spell check.
 
 // first creating instance variables
 String[] text;  // variable to hold last checked text instance in document
-Word_Object wordBuffer = new Word_Object();
+Linkedlist wordBuffer = new Linkedlist();  //linked list to hold our word object list (for the entire document)
+
+public void populateLinkedList(String[] text){
+  Word_object curr = new Word_Object();
+  int first = 1;
+  while(text.hasnext()){
+    int last = text(i).length();
+    if(text(last).equals(".") || text(last).equals("?") || text(last).equals("!")){
+      //means the sentence has ended, add boolean for end of sentence for capital check
+      //adding the capital check
+      first = 1;
+      //adding word object to linked list
+      
+    }else{
+      //means no capital case, adding to linked list. checking if first int = 1 --> meaning the first word of the sentence
+      if(first == 1){
+        //resetting the value
+        first = 0;
+
+        //adding to linked list with capital
+        
+      }else{
+        //just add the word to linked list, no capital check
+        
+      }
+    }
+  }
+  //incrementing the counter
+  i += 1;
+}
+
 
 
 }
