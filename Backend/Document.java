23bd63f9e76790b32a0eca97c94ba0939fe35b0d
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-13 17:45:50 -0500

    Update Document.java
    added to linked list loop, pretty sure is complete.

--- Backend/Document.java
+++ Backend/Document.java
@@ -6,28 +6,29 @@ public class Document extends Word_Object{
 String[] text;  // variable to hold last checked text instance in document
 Linkedlist wordBuffer = new Linkedlist();  //linked list to hold our word object list (for the entire document)
 
-public void populateLinkedList(String[] text){
+public void populateLinkedList(String[] text, Linkedlist wordBuffer){
   int first = 1;
   while(text.hasnext()){
+    
     int last = text(i).length();
     if(text(last).equals(".") || text(last).equals("?") || text(last).equals("!")){
       //means the sentence has ended, add boolean for end of sentence for capital check
       //adding the capital check
+      Word_Object curr = new Word_Object(text(i),0,1,0,0,0,0,0,0,0,0);
       first = 1;
       //adding word object to linked list
-      Word_Object curr = new Word_Object(text(i),     
+      wordBuffer.add(curr);
       
     }else{
       //means no capital case, adding to linked list. checking if first int = 1 --> meaning the first word of the sentence
       if(first == 1){
         //resetting the value
         first = 0;
-
         //adding to linked list with capital
-        
+        Word_Object curr = new Word_Object(text(i),1,0,0,0,0,0,0,0,0,0);
       }else{
         //just add the word to linked list, no capital check
-        
+        Word_Object curr = new Word_Object(text(i),0,0,0,0,0,0,0,0,0,0);
       }
     }
   }
