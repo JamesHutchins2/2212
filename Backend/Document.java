c256e03b48777f7a4aabfb82cd684a25c52cc481
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-13 13:55:55 -0500

    Update Document.java

--- Backend/Document.java
+++ Backend/Document.java
@@ -1,1 +1,10 @@
+Class Document extends Word_Object{
+//this class is to work as the hub for the spell check system creating the linked list from the workspace text.
+//It transforms the data types into our word object so that the words can be analyzed by the spell check.
 
+// first creating instance variables
+String[] text;  // variable to hold last checked text instance in document
+Word_Object wordBuffer = new Word_Object();
+
+
+}
