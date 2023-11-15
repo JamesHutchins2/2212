3df020a246ad2f2df85377679bbf4fe904cc13c3
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-15 17:34:25 -0500

    Update Doc_Analysis.java
    added loops to scan

--- Backend/Doc_Analysis.java
+++ Backend/Doc_Analysis.java
@@ -1,4 +1,39 @@
 Class Doc_Analysis{
+  //this class is to return the analysis portion 
+  
+  //defining instance variables
+  char[] text;
+  int charCount = 0;
+  int wordCount = 0;
+  int lineCount = 0;
 
+  //method to return char count
+  private int getCharCount(char[] text){
+    //the size of the array is the total char count
+    charCount = text.size();
+  }
+
+  //to return word count
+  private int getWordCount(char[] word){
+    //loop through char array
+    while(text.hasnext()){
+      //checking if space, period, means new word
+      if(text(i).equals(" ") || text(i).equals(".") || text(i).equals("?") || text(i).equals("!")){
+        wordCount += 1;
+      }
+    }
+  }
+
+
+  //to return line count
+  private int getLineCount(char[] text){
+    //loop through entire file
+    while(text.hasnext()){
+      //if newline, count
+      if(char[i].equals(\n)){
+         lineCount += 1;
+      }
+    }
+  }
   
 }
