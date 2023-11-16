7bbfb694e340de3927be991e506ba20002487f8c
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-15 19:58:42 -0500

    Update and rename Manage_File.java to manageFile.java

---
+++ RunTime/manageFile.java
@@ -0,0 +1,14 @@
+class manageFile{
+  //this is a class that works with the UI to open a file, save a file, etc.
+  public String[] text;
+  private String path;
+
+  private void fileToString(String path){
+    
+  }
+
+  private void stringToFile(String[] text){
+    
+  }
+  
+}
