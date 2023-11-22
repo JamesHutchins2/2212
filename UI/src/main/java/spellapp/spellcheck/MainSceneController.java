9cf8c805787cf98576da7a592e4a845c07d4924c
michelle <97324440+mbourdo@users.noreply.github.com>
2023-11-21 21:12:57 -0500

    Add files via upload

--- UI/src/main/java/spellapp/spellcheck/MainSceneController.java
+++ UI/src/main/java/spellapp/spellcheck/MainSceneController.java
@@ -119,15 +119,14 @@ public class MainSceneController {
 
     @FXML
     void startSpell(ActionEvent event) {
-        StringBuilder content = new StringBuilder () ;
-        content.append ( textArea .getText() ) ;
-        // Call Backend methods to load linked Lists, etc before starting spell check mode
+        StringBuilder content = new StringBuilder();
+        content.append( textArea.getText() );
+        mainLabel.setText("Spell Check Mode");
 
+        // Start Spell check mode
+        // Pass the "content" String into Backend methods to load linked Lists, etc before starting spell check mode
         // Once done - Parse linked list of incorrect words and somehow show them differently on UI
-        
-        // Then we want to create a series of popup UIs that are able to popup when certain words are clicked on
-
-
+        // THen we want to create a series of popup UIs that are able to popup when certain words are clicked on
     }
 
 }
