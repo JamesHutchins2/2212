bda8b4564bf4cf45869e35d91ac136a1b8ae8aba
michelle <97324440+mbourdo@users.noreply.github.com>
2023-11-29 18:50:15 -0500

    Javadoc comments

--- UI/src/main/java/spellapp/spellcheck/HelpController.java
+++ UI/src/main/java/spellapp/spellcheck/HelpController.java
@@ -14,10 +14,24 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * @author      James Hutchins
+ * @author      Michelle Bourdon
+ * @author      Jessica Kerr
+ * @author      Laila El attar
+ * @author      Nouran Sakr
+ * @version     1.0
+ * @since       0.0
+ * The HelpController class handles actions related to the help functionality in the application.
+ */
 public class HelpController {
     @FXML
     private BorderPane help;
 
+    /**
+     * Handles the "Close Help" button click event to close the help window.
+     * @param event The ActionEvent triggered by the "Close Help" button.
+     */
     @FXML
     void CloseHelp(ActionEvent event) {
         help.getScene().getWindow().hide();
