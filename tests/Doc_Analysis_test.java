6ff3d5e18cebf19cda8c4ce7cdc94d26487d0b95
Jessica Kerr <123105251+jkerr87@users.noreply.github.com>
2023-11-29 12:25:12 -0500

    fixed tests - final

--- tests/Doc_Analysis_test.java
+++ tests/Doc_Analysis_test.java
@@ -1,7 +1,9 @@
 package tests;
 import Backend.Document;
+import Backend.Word_Object;
 import Backend.Doc_Analysis;
 import Backend.Doc_Error;
+
 public class Doc_Analysis_test {
 
     public void testDocAnalysis() {
@@ -9,16 +11,16 @@ public class Doc_Analysis_test {
 
     //create a linked list of words
 
-    String [] test_corpus = {"The", "Quick", "brown", "fox", "Jumps", "over", "the", "lazy", "dog.", "\n",
-        "it's", "a", "beautifull", "sunny", "day", "outside.", "\n",
-        "Tommorrow", "will", "be", "an", "intersting", "day", "for", "our", "picnic.", "\n",
-        "She", "said", "her", "favoritte", "book", "is", "\"The", "Great", "Gatsby\".", "\n",
-        "Could", "you", "pleese", "pass", "the", "ketchup?", "\n",
-        "He", "decided", "to", "seperate", "the", "papers", "into", "two", "piles.", "\n",
-        "Their", "going", "to", "visit", "the", "museum", "next", "week.", "\n",
-        "The", "cat", "chased", "it's", "tail", "around", "the", "living", "room.", "\n",
-        "We", "recieved", "an", "invitation", "for", "the", "gallery", "opening.", "\n",
-        "He", "loves", "to", "play", "the", "guitar", "and", "the", "pianoo."};
+    String test_corpus = "The Quick brown fox Jumps over the lazy dog.\n" +
+    "it's a beautifull sunny day outside.\n" +
+    "Tommorrow will be an an intersting day for our picnic.\n" +
+    "She said her favoritte book is \"The Great Gatsby\".\n" +
+    "Could you pleese pass the ketchup?\n" +
+    "He decided to seperate the papers into two piles.\n" +
+    "Their going to visit the museum next week.\n" +
+    "The cat chased it's tail around the living room.\n" +
+    "We recieved an invitation for the gaLlery opening.\n" +
+    "He loves to play the guitar and the pianoo.";
 
 
 
@@ -27,10 +29,10 @@ public class Doc_Analysis_test {
     Document doc = new Document(test_corpus);
 
     //now create doc analysis object, which will in turn create a linked list of words
-    Doc_Analysis doc_analysis = new Doc_Analysis(doc);
+    Doc_Analysis doc_analysis = new Doc_Analysis(doc.wordBuffer);
 
     //test get line count
-    int lc = doc_analysis.getLineCount();
+    int lc = doc_analysis.get_line_count();
 
     //correct metric of lines given by the test corpus is 10
     if(lc == 10){
@@ -40,7 +42,7 @@ public class Doc_Analysis_test {
     }
 
     //test get word count
-    int wc = doc_analysis.getWordCount();
+    int wc = doc_analysis.get_word_count();
     //correct metric of words given by the test corpus is 80
     if(wc == 80){
         System.out.println("Test 2: Passed");
@@ -49,7 +51,7 @@ public class Doc_Analysis_test {
     }
 
     //test get char count
-    int cc = doc_analysis.getCharCount();
+    int cc = doc_analysis.get_char_count();
     //correct metric of chars given by the test corpus is 399
     if(cc == 399){
         System.out.println("Test 3: Passed");
