5a124a530355d0f0bc71ae736f8cc91470c64c3f
michelle <97324440+mbourdo@users.noreply.github.com>
2023-11-29 18:41:20 -0500

    Javadoc comments

--- UI/src/main/java/spellapp/spellcheck/ExitController.java
+++ UI/src/main/java/spellapp/spellcheck/ExitController.java
@@ -14,13 +14,35 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-
+/**
+ * @author      James Hutchins
+ * @author      Michelle Bourdon
+ * @author      Jessica Kerr
+ * @author      Laila El attar
+ * @author      Nouran Sakr
+ * @version     1.0
+ * @since       0.0
+ * The ExitController class handles actions related to exiting the application.
+ */
 public class ExitController {
+    /**
+     * Handles the "Exit" button click event to exit the application.
+     * @param event The ActionEvent triggered by the "Exit" button.
+     */
     @FXML
     void exitExit(ActionEvent event) {
         Platform.exit();
     }
 
+    /**
+     * Handles the "Save" button click event to save data and exit the application.
+     * This method performs the following steps:
+     *     Brings up the Save popup.
+     *     Allows the user to set the filename and path.
+     *     Performs the save operation.
+     *     Exits the application.
+     * @param event The ActionEvent triggered by the "Save" button.
+     */
     @FXML
     void exitSave(ActionEvent event) {
         // Basically do same as if the save button was clicked
